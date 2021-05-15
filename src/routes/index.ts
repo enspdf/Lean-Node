2329b1dc8420159024b3508bd379eb83a5a3a5f2
Sebastian Higuita <sebastianhiguitaca@gmail.com>
2021-05-15 13:44:16 -0500

    [NEW]: Base route file added with module prefixes

---
+++ src/routes/index.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express'
+
+import technologyRouter from '../modules/technology/router'
+import staffRouter from '../modules/staff/router'
+import userRouter from '../modules/user/router'
+import authenticationRouter from '../modules/authentication/router'
+
+const routes = Router()
+
+routes.use('/technology', technologyRouter)
+routes.use('/staff', staffRouter)
+routes.use('/user', userRouter)
+routes.use('/authentication', authenticationRouter)
+
+export default routes
