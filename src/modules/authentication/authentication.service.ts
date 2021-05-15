39879382057c92e7a11738ea6859d3678a4ae87c
Sebastian Higuita <sebastianhiguitaca@gmail.com>
2021-05-15 14:34:52 -0500

    [UPDATE]: Add docs to the authentication functions including the flows.

--- src/modules/authentication/authentication.service.ts
+++ src/modules/authentication/authentication.service.ts
@@ -10,6 +10,13 @@ import { IRegister } from './interfaces/IRegister'
 import { sendEmail } from '../../utils/sendEmail'
 
 export default class AuthenticationService {
+  /**
+   * Method used to take the user registration information, check the roles and bases on that
+   * create a new user, create a token and return it.
+   *
+   * @param userInformation
+   * @returns
+   */
   static register = async (userInformation: IRegister): Promise<IAuthenticationResponse> => {
     const { role } = userInformation
 
@@ -32,6 +39,13 @@ export default class AuthenticationService {
     }
   }
 
+  /**
+   * Method used to take the user login and password information, tries to math the password
+   * and if success cretes a new token and return it in the response.
+   *
+   * @param credentials
+   * @returns
+   */
   static login = async (credentials: ILogin): Promise<IAuthenticationResponse> => {
     const user = await User.findOne({ where: { email: credentials.email } })
 
@@ -53,6 +67,11 @@ export default class AuthenticationService {
     }
   }
 
+  /**
+   * Method used to update the password to the user if forgot the old one.
+   *
+   * @param email
+   */
   static forgotPassword = async (email: string): Promise<void> => {
     const user = await User.findOne({ where: { email } })
 
@@ -60,13 +79,18 @@ export default class AuthenticationService {
       throw new NotFoundError('We could not found an user with provided credentials')
     }
 
+    // After check and found the user generates a new password of 15 chars wihh faker.
     const newPassword = faker.internet.password(15, false)
+
+    // Hash the password generated previously to save-it again hashed in the database
     const hashedPassword = await bcrypt.hash(newPassword, 10)
 
     user.password = hashedPassword
 
     await user.save()
 
+    // Once the email has been successfully updated, the system sends an email to the user
+    // with the new password used to login in the account
     await sendEmail({
       email: user.email,
       subject: 'New Password Request',
